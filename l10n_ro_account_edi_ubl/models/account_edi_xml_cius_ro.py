d0977e635384afad1be647bff5da726cbb868c95
Fekete Mihai <feketemihai@nexterp.ro>
2024-12-27 11:09:56 +0200

    Add edi ubl fizical persons

--- l10n_ro_account_edi_ubl/models/account_edi_xml_cius_ro.py
+++ l10n_ro_account_edi_ubl/models/account_edi_xml_cius_ro.py
@@ -32,10 +32,18 @@ class AccountEdiXmlCIUSRO(models.Model):
             vals["city_name"] = partner.city.upper().replace(" ", "")
         return vals
 
+    def _get_partner_party_vals(self, partner, role):
+        # EXTENDS account.edi.xml.ubl_21
+        vals = super()._get_partner_party_vals(partner, role)
+        partner = partner.commercial_partner_id
+        if not partner.is_company and not partner.l10n_ro_edi_ubl_no_send_cnp:
+            vals["endpoint_id"] = "0000000000000"
+        return vals
+
     def _get_partner_party_tax_scheme_vals_list(self, partner, role):
         # EXTENDS account.edi.xml.ubl_21
         vals_list = super()._get_partner_party_tax_scheme_vals_list(partner, role)
-
+        partner = partner.commercial_partner_id
         for vals in vals_list:
             # /!\ For Romanian companies, the company_id can be with or without country code.
             if (
@@ -44,7 +52,18 @@ class AccountEdiXmlCIUSRO(models.Model):
                 and not partner.vat.upper().startswith("RO")
             ):
                 vals["tax_scheme_id"] = "!= VAT"
+            if not partner.is_company and not partner.l10n_ro_edi_ubl_no_send_cnp:
+                vals["company_id"] = "0000000000000"
         return vals_list
+
+    def _get_partner_party_legal_entity_vals_list(self, partner):
+        val_list = super()._get_partner_party_legal_entity_vals_list(partner)
+        partner = partner.commercial_partner_id
+        if not partner.is_company and not partner.l10n_ro_edi_ubl_no_send_cnp:
+            for vals in val_list:
+                if vals.get("commercial_partner") == partner:
+                    vals["company_id"] = "0000000000000"
+        return val_list
 
     def _get_tax_category_list(self, invoice, taxes):
         partner = invoice.company_id.partner_id
